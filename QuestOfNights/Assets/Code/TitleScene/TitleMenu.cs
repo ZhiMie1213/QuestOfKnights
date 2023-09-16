a7e8c06dc5641b4676a433136ce5b2ad40573e58
FuNeKoU <87838131+ZhiMie1213@users.noreply.github.com>
2023-09-16 11:15:53 +0900

    09/16

--- QuestOfNights/Assets/Code/TitleScene/TitleMenu.cs
+++ QuestOfNights/Assets/Code/TitleScene/TitleMenu.cs
@@ -8,7 +8,7 @@ public class TitleMenu : MonoBehaviour
     
     void Update()
     {
-        if ( Input.GetKey( KeyCode.Space ) )
+        if ( Input.anyKey )
         {
             Invoke( "LoadScene", 0.2f );
         }
