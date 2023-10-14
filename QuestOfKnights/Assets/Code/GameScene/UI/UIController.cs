7d352bd78d86993ab9f90e47c756712accfb42aa
FuNeKoU <87838131+ZhiMie1213@users.noreply.github.com>
2023-10-14 16:39:20 +0900

    10/14

--- QuestOfKnights/Assets/Code/GameScene/UI/UIController.cs
+++ QuestOfKnights/Assets/Code/GameScene/UI/UIController.cs
@@ -8,42 +8,48 @@ using UnityEngine.SceneManagement;
 public class UIController : MonoBehaviour
 {
     public static UIController instance;
-    private void Awake ( )
+
+    private void Awake()
     {
         instance = this;
     }
 
     public Slider explvlSlider;
     public TMP_Text expLvlText;
-    
+
     public TMP_Text timeText;
-    
+
     public GameObject levelEndScreen;
-    
+
     public LevelUpSelectionButton[] levelUpButtons;
     public GameObject levelUpPanel;
 
     public string mainMenuName;
-    
-    void Start ( )
+
+    public GameObject pauseScreen;
+
+    void Start( )
     {
-        
+
     }
 
-    void Update ( )
+    void Update( )
     {
-        
+        if ( Input.GetKeyDown(KeyCode.Escape ) )
+        {
+            PauseUnpause( );
+        }
     }
 
-    public void UpdateExperience ( int currentExp, int levelExp, int currentLvl )
+    public void UpdateExperience( int currentExp, int levelExp, int currentLvl )
     {
         explvlSlider.maxValue = levelExp;
         explvlSlider.value = currentExp;
-        
+
         expLvlText.text = "Lv: " + currentLvl;
     }
 
-    public void UpdateTimer ( float time )
+    public void UpdateTimer( float time )
     {
         float minutes = Mathf.FloorToInt( time / 60f );
         float seconds = Mathf.FloorToInt( time % 60 );
@@ -51,13 +57,37 @@ public class UIController : MonoBehaviour
         timeText.text = minutes + ":" + seconds.ToString( "00" );
     }
 
-    public void GoToMainMenu ( )
+    public void GoToMainMenu( )
     {
         SceneManager.LoadScene( mainMenuName );
+        Time.timeScale = 1f;
     }
 
-    public void Restart ( )
+    public void Restart( )
     {
         SceneManager.LoadScene( SceneManager.GetActiveScene( ).name );
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame( )
+    {
+        Application.Quit( );
+    }
+
+    public void PauseUnpause( )
+    {
+        if ( pauseScreen.activeSelf == false )
+        {
+            pauseScreen.SetActive( true );
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseScreen.SetActive( false );
+            if ( pauseScreen.activeSelf == false )
+            {
+                Time.timeScale = 1f;
+            }
+        }
     }
 }
